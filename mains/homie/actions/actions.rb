163741f56b659944173424c2db3168726c49a8af
James Scott <JScott@brotherhoodmutual.com>
2019-03-14 09:46:36 -0400

    Adding Small Action Objects

---
+++ mains/homie/actions/actions.rb
@@ -0,0 +1,13 @@
+# File: ./main/homie/commands/commands.rb
+
+# ##
+# Each Directory has a same named file that handles it's includes
+# ##
+
+
+module Homie
+  module Actions
+  end
+end
+
+require_relative 'action'
---
+++ mains/homie/actions/firmware_delete.rb
@@ -0,0 +1,20 @@
+
+
+module Homie
+  module Handlers
+
+    class FirmwareDelete
+
+      def self.call(params)
+        new().call(params)
+      end
+
+      def initialize()
+      end
+
+      def call(params)
+        # do work
+      end
+    end
+  end
+end
